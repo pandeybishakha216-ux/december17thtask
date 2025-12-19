d5af816c1210d1632eacf5ffbbfd4764695f8cf8
Bishakha Pandey <pandeybishakha216@gmail.com>
2025-12-19 15:54:45 +0545

    Update list to replace second element with 'a'
    Modified the list to replace the second element with 'a' and adjusted the list accordingly.

--- qn11.py
+++ qn11.py
@@ -1,7 +1,8 @@
 #Given list is [1,2,3,4] but expected output is [1,"a",2,4]
-list = [1, 2, 3, 4]
-for i in range(len(list)):
-    if i == 1:
-        print("a")
-    print(list[i])
-    
+list_6 = [1, 2, 3, 4]
+for i in range(2):
+    if list_6[i] == 2:
+        list_6[i] = "a"
+        list_6.pop(i+1)
+        list_6.insert(2,2)
+print(list_6) 
